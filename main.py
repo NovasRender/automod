19736127fcbae1afade635250601cd4daf3e73ba
NovasRender <henrylambert10@hotmail.com>
2024-11-05 17:13:24 +1030

    Update main.py

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ async def addflag(ctx, *, message):
   embed = discord.Embed(title="Add Flag", description=f"Successfully added a {flagtype} flag! (flagged {word} : db {message})!", color=0x00ff00)
 
   if flagtype == "default":
-    for x in range(0,len(baddef):
+    for x in range(0, len(baddef):
       add_flagged_word(ctx.guild.id, baddef[x])
   
   add_flagged_word(ctx.guild.id, message)
